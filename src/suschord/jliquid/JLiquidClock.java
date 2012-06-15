a916a970554789bb0db59d5a48f2bd2d041c362f
Jacob Peck <suschord@suspended-chord.info>
2012-06-15 18:49:52 -0400

    Initial clock definition.

--- src/suschord/jliquid/JLiquidClock.java
+++ src/suschord/jliquid/JLiquidClock.java
@@ -4,6 +4,44 @@
 
 package suschord.jliquid;
 
-public class JLiquidClock {
+import java.util.*;
+import suschord.jliquid.*;
 
+public class JLiquidClock {
+  private ArrayList<JLiquidTask> tasks;
+  private boolean running = false;
+  private int tickNum = 0;
+  
+  private boolean tickEdge() { return this.tickNum % 2 == 0; }
+  public boolean posEdge() { return this.tickEdge(); }
+  public boolean negEdge() { return !this.tickEdge(); }
+  
+  public void registerTask(JLiquidTask task) { this.tasks.add(task); }
+  public void deregisterTask(JLiquidTask task) { this.tasks.remove(task); }
+  
+  public void start() { this.run(); }
+  public void stop() { this.running = false; }
+  
+  private void run() {
+    this.running = true;
+    while(this.running && this.workToDo())
+      this.tick();
+  }
+  
+  private boolean workToDo() {
+    if (this.tasks.size() == 0) return false;
+    for (JLiquidTask t : this.tasks)
+      if (t.workToDo()) return true;
+    return false;
+  }
+  
+  private void tick() {
+    this.tickNum++;
+    if (this.negEdge())
+      for (JLiquidTask t : this.tasks)
+        t.load();
+    else
+      for (JLiquidTask t : this.tasks)
+        t.calculate();
+  }
 }
